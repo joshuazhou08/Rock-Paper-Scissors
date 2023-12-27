f3a896e79291077c2f6595f0d9e02aa986dc1c3a
joshuazhou08 <joshuazhou0808@gmail.com>
2023-12-27 12:37:20 -0800

    Finish UI And Interactions
    The result text now displays wins/losses and the computer's choice is
    displayed.

--- game.js
+++ game.js
@@ -4,14 +4,17 @@ function getComputerChoice(){
     const randNumber = Math.random();
     //If randNumber is less than 0.33, return "rock"
     if (randNumber < 0.33) {
+        computerChoice.setAttribute('src', 'images/Rock-Flat-icon.png')
         return "rock"
     }
     //If randInt is greater than or equal to 0.33 but less than 0.66, return "paper"
     else if (randNumber < 0.66) {
+        computerChoice.setAttribute('src', 'images/Paper.png')
         return "paper";
     }
     //If randNumber is greater than or equal to 0.66 but less than 1, return "scissors"
     else {
+        computerChoice.setAttribute('src', 'images/Scissor-icon.png')
         return "scissors";
     }
 
@@ -23,43 +26,43 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     const playerChoice = playerSelection.toLowerCase()
     if (computerSelection === "rock"){
         if (playerChoice === "rock"){
-            console.log("Tie! Both chose rock. Replay.");
+            result.textContent = ("Tie! Both chose rock. Replay.");
         }
         else if (playerChoice === "paper"){
-            console.log(`You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${computerSelection}`) ;
+            result.textContent = (`You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${computerSelection}`) ;
             playerScore++
         }
         else {
-            console.log(`You lose! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} loses to ${computerSelection}`);
+            result.textContent = (`You lose! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} loses to ${computerSelection}`);
             computerScore++
         }
 
     }
     else if (computerSelection === "paper"){
         if (playerChoice === "rock"){
-            console.log(`You lose! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} loses to ${computerSelection}`);
+            result.textContent = (`You lose! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} loses to ${computerSelection}`);
             computerScore++
         }
         else if (playerChoice === "paper"){
-            console.log("Tie! Both chose paper. Replay.");
+            result.textContent = ("Tie! Both chose paper. Replay.");
         }
         else {
-            console.log(`You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${computerSelection}`);
+            result.textContent = (`You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${computerSelection}`);
             playerScore++
         }
 
     }
     else if (computerSelection === "scissors") {
         if (playerChoice === "rock"){
-            console.log(`You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${computerSelection}`) ;
+            result.textContent = (`You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${computerSelection}`) ;
             playerScore++
         }
         else if (playerChoice === "paper"){
-            console.log(`You lose! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} loses to ${computerSelection}`);
+            result.textContent = (`You lose! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} loses to ${computerSelection}`);
             computerScore++
         }
         else {
-            console.log("Tie! Both chose scissors. Replay.");
+            result.textContent = ("Tie! Both chose scissors. Replay.");
         }
     }
     playerScoreDisp.textContent = `Player: ${playerScore}`
@@ -68,11 +71,14 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
 //Initialize the scores in the variables playerScore and computerScore
 let playerScore = 0;
 let computerScore = 0;
+//Make the scores section dynamic to show the results
 const result = document.querySelector(".score > #results")
 const  playerScoreDisp = document.querySelector(".playerScore")
 const  computerScoreDisp = document.querySelector(".computerScore")
 playerScoreDisp.textContent = `Player: ${playerScore}`
 computerScoreDisp.textContent = `Computer: ${computerScore}`
+//Display the computer's choice
+const computerChoice = document.querySelector('.computer-choice')
 //Make buttons reponsive to clicks
 const rockButton = document.querySelector("#rock-btn")
 const paperButton = document.querySelector("#paper-btn")
@@ -80,3 +86,4 @@ const scissorButton = document.querySelector('#scissor-btn')
 rockButton.addEventListener('click', function(){playRound("rock", getComputerChoice())})
 paperButton.addEventListener('click',function(){playRound("paper", getComputerChoice())})
 scissorButton.addEventListener('click', function(){playRound("scissors", getComputerChoice())})
+
